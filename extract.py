4f6c42358ccb0a348cf596ffc924a49dc3d0688f
andreignatius <andre.callme@gmail.com>
2024-05-12 22:08:34 +0800

    logreg model first cut

--- extract.py
+++ extract.py
@@ -8,7 +8,9 @@ from scipy.signal import find_peaks
 from gtda.homology import VietorisRipsPersistence
 from gtda.plotting import plot_diagram
 import numpy as np
-from base_model import BaseModel  # Assuming the BaseModel class is in a separate file named 'base_model.py'
+# from base_model import BaseModel  # Assuming the BaseModel class is in a separate file named 'base_model.py'
+from logreg_model import LogRegModel
+from trading_strategy import TradingStrategy
 
 def fetch_data(tickers, start_date, end_date):
     """Fetches closing prices for given tickers from Yahoo Finance."""
@@ -24,17 +26,61 @@ if __name__ == "__main__":
     start_date = '2013-01-01'
     end_date = '2023-01-01'
     raw_data = fetch_data(tickers, start_date, end_date)
+    interest_costs_total = []
+    transaction_costs_total = []
+    final_portfolio_values = []
+    trade_logs = []
 
     if not raw_data.empty:
         # Assume file_path is where you save the fetched data temporarily
         raw_data.to_csv('temp_data.csv')
         
         # Initialize and use the BaseModel for advanced analysis
-        model = BaseModel(file_path='temp_data.csv', train_start='2013-01-01', train_end='2018-01-01', test_start='2018-01-01', test_end='2023-01-01')
+        # model = BaseModel(file_path='temp_data.csv', train_start='2013-01-01', train_end='2018-01-01', test_start='2018-01-01', test_end='2023-01-01')
+        model = LogRegModel(file_path='temp_data.csv', train_start='2013-01-01', train_end='2018-01-01', test_start='2018-01-01', test_end='2023-01-01')
         model.load_preprocess_data()  # Load and preprocess the data
         model.train_test_split_time_series()  # Split data into training and testing
         model.train()  # Placeholder for training method
-        test_set = model.retrieve_test_set()
+
+        data = model.retrieve_test_set()
+        # predicted_categories = model.predict()
+
+        # Perform backtesting, log trades, calculate final portfolio value
+        # ... [Your backtesting logic here] ...
+        # Backtesting with stop-loss and take-profit
+        # Instantiate the TradingStrategy class
+        # trading_strategy = TradingStrategy(model, data, leverage_factor=leverage_factor, annual_interest_rate=annual_interest_rate)
+        trading_strategy = TradingStrategy(model, data)
+        # Run the trading strategy with the model's predictions
+        trading_strategy.execute_trades()
+
+        # Retrieve results and output
+        trading_results = trading_strategy.evaluate_performance()
+
+        trade_log = trading_results['Trade Log']
+        final_portfolio_value = trading_results['Final Portfolio Value']
+        pnl_per_trade = trading_results['Profit/Loss per Trade']
+        interest_costs = sum(trading_results['Interest Costs'])
+        transaction_costs = trading_results['Transaction Costs']
+        print("interest_costs111: ", interest_costs)
+        print("transaction_costs111: ", transaction_costs)
+
+        interest_costs_total.append( interest_costs )
+        transaction_costs_total.append( transaction_costs )
+
+        # Output
+        print(trade_log)
+        print("num trades: ", len(trade_log))
+        print(f"Final Portfolio Value Before Cost: {final_portfolio_value}")
+        final_portfolio_value = final_portfolio_value - ( interest_costs + transaction_costs )
+        print(f"Final Portfolio Value After Cost: {final_portfolio_value}")
+
+        # pnl_per_trade = ( final_portfolio_value - starting_cash ) / len(trade_log)
+        print("PnL per trade: ", pnl_per_trade)
+
+        # Collect results
+        trade_logs.append(trade_log)
+        final_portfolio_values.append(final_portfolio_value)
 
         # # Use the processed data for further analysis
         # plot_data(model.data)  # Plot the data with additional features
---
+++ logreg_model.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from scipy.fft import fft
+import numpy as np
+from scipy.signal import find_peaks
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import cross_val_score
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.utils.class_weight import compute_class_weight
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+import random
+
+from base_model import BaseModel
+
+class LogRegModel(BaseModel):
+    def __init__(self, file_path, train_start, train_end, test_start, test_end):
+        super().__init__(file_path, train_start, train_end, test_start, test_end)
+        # Elastic Net parameters
+        l1_ratio = 0.5  # L1 weight in the range [0,1]. 0 is L2, 1 is L1.
+        alpha = 1.0     # Regularization strength. Higher values mean more regularization.
+        self.model = LogisticRegression(class_weight='balanced', penalty='elasticnet', l1_ratio=l1_ratio, C=1/alpha, solver='saga', max_iter=1000)
+
+    def train(self):
+        self.train_test_split_time_series()
+
+        print("y_train value counts: ", self.y_train.value_counts())
+        # self.X_train_scaled = self.scaler.fit_transform(self.X_train)
+        # self.X_test_scaled = self.scaler.transform(self.X_test)
+
+        # self.model.fit(self.X_train_scaled, self.y_train)
+        print("check X train: ", self.X_train)
+        print("check y train: ", self.y_train)
+        y_train = self.y_train['USDBRL=X']
+        print("check y_train2: ", y_train)
+        self.model.fit(self.X_train, y_train)
+
+
+    def predict(self):
+        # self.data['PredictedLabel'] = self.model.predict(self.scaler.transform(self.X))
+        # return self.data
+        # predicted_categories = self.model.predict(self.scaler.transform(self.X_test_scaled))
+        predicted_categories = self.model.predict(self.X_test)
+        # print("CHECK predicted_labels: ", predicted_categories)
+        return predicted_categories
+
+    def evaluate(self, X, y):
+        # Implement evaluation logic
+        pass
+
+
+
